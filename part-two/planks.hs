acef98869a12c3781b3639e32cf8122fa143f55d
Ludvig Gunne Lindström <ludvig.gunne.lindstrom@proton.me>
2024-02-03 17:29:41 +0100

    Faster planks

--- part-two/planks.hs
+++ part-two/planks.hs
@@ -2,13 +2,28 @@
 -- Kattis problem: The Plank
 -- Submission ID: 12849824
 
-planks :: Integer -> Integer
-planks 0 = 1
-planks n | n < 0 = 0
-         | n > 0 = (planks (n - 1)) + (planks (n - 2)) + (planks (n - 3))
+{-
+    Problem: Determine the number of ways to construct a plank
+    of length n, using smaller planks of lengths 1, 2 and 3.
+
+    The answer is computed by adding the number of ways to build
+    planks of length n - 1, n - 2, and n - 3.
+ -}
+
+build :: Integer -> [Integer]
+build 0 = [1]
+build 1 = [1, 1]
+build 2 = [2, 1, 1]
+build n = p : prev
+    where
+        p    = p' + p'' + p'''
+        p'   = prev !! 0
+        p''  = prev !! 1
+        p''' = prev !! 2
+        prev = build $ n - 1
 
 main :: IO ()
 main = do
     input <- getLine
-    let ans = planks (read input :: Integer)
+    let ans = head . build $ read input :: Integer
     print ans
